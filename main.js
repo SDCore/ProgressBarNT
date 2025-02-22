9d4dc98d3824abb0506fd6107d58b756b49a7bba
SDCore <sdcored@gmail.com>
2025-02-22 00:27:49 -0600

    Styles & Updates
    - Settling on a base design idea
    - Base logic for counting seconds/minutes/hours/days

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
-// get the current second of the current minute and translate that into a percentage of 60
-var secondProgressBar = document.getElementById('secondProgressBar-Inner');
-var hourProgressBar = document.getElementById('hourProgressBar-Inner');
+var secondProgressBar = document.getElementById('secondProgressBar');
+var minuteProgressBar = document.getElementById('minuteProgressBar');
+var hourProgressBar = document.getElementById('hourProgressBar');
+var dayProgressBar = document.getElementById('dayProgressBar');
 
-// interate every second to update the progress bar and reset it to 0 when it reaches 100
+function daysInThisMonth() {
+	var now = new Date();
+	return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+}
+
 setInterval(function () {
 	currentSecond = new Date().getSeconds();
-	currentSecondPercentage = (currentSecond / 60) * 100;
-	secondProgressBar.style.width = currentSecondPercentage + '%';
+	currentMinute = new Date().getMinutes();
+	currentHour = new Date().getHours();
+	currentDay = new Date().getDate();
+
+	currentSecondPercentage = (currentSecond / 60) * 100 + 1;
+	currentMinutePercentage = (currentMinute / 60) * 100 + 1;
+	currentHourPercentage = (currentHour / 24) * 100;
+	currentDayPercentage = (currentDay / daysInThisMonth()) * 100;
+
+	// secondProgressBar.style.width = currentSecondPercentage + '%';
+	secondProgressBar.style.width = currentSecondPercentage < 99 ? currentSecondPercentage + '%' : '100%';
+	minuteProgressBar.style.width = currentMinutePercentage < 99 ? currentMinutePercentage + '%' : '100%';
+	hourProgressBar.style.width = currentHourPercentage < 95 ? currentHourPercentage + '%' : '100%';
+	dayProgressBar.style.width = currentDayPercentage + '%';
 }, 1000);
-
-// interate every second to update the progress bar and reset it to 0 when it reaches 100
-setInterval(function () {
-	currentHour = new Date().getMinutes();
-	currentHourPercentage = (currentHour / 60) * 100;
-	hourProgressBar.style.width = currentHourPercentage + '%';
-}, 1000);
-
-// function progressBarTimer(name, increment, interval, duration) {
-// 	var progress = 0;
-// 	var progressBar = document.getElementById(name);
-
-// 	var intervalId = setInterval(function () {
-// 		progress += increment;
-// 		progressBar.style.width = progress + '%';
-
-// 		if (progress > 100) {
-// 			progress = 0;
-// 		}
-// 	}, interval);
-// }
-
-// progressBarTimer('secondProgressBar-Inner', 100 / 60, 1000, 60000);
