1f034ebde68bf229641263ec72cd9c87ae57fc6e
SDCore <sdcored@gmail.com>
2025-03-06 03:07:28 -0600

    Add Text for Progress Bars

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var secondProgressBar = document.getElementById('secondProgressBar');
+var secondText = document.getElementById('secondText');
+
 var minuteProgressBar = document.getElementById('minuteProgressBar');
+var minuteText = document.getElementById('minuteText');
+
 var hourProgressBar = document.getElementById('hourProgressBar');
+var hourText = document.getElementById('hourText');
+
 var dayProgressBar = document.getElementById('dayProgressBar');
+var dayText = document.getElementById('dayText');
+
 var monthProgressBar = document.getElementById('monthProgressBar');
+var monthText = document.getElementById('monthText');
+
 var decadeProgressBar = document.getElementById('decadeProgressBar');
+var decadeText = document.getElementById('decadeText');
+
 var centuryProgressBar = document.getElementById('centuryProgressBar');
+var centuryText = document.getElementById('centuryText');
+
 var millenniumProgressBar = document.getElementById('millenniumProgressBar');
+var millenniumText = document.getElementById('millenniumText');
 
 function daysInThisMonth() {
 	var now = new Date();
@@ -30,11 +45,26 @@ setInterval(function () {
 	currentMillenniumPercentage = ((currentYear % 1000) / 1000) * 100;
 
 	secondProgressBar.style.width = currentSecondPercentage + '%';
+	secondText.innerHTML = currentSecond;
+
 	minuteProgressBar.style.width = currentMinutePercentage + '%';
+	minuteText.innerHTML = currentMinute;
+
 	hourProgressBar.style.width = currentHourPercentage + '%';
+	hourText.innerHTML = currentHour;
+
 	dayProgressBar.style.width = currentDayPercentage + '%';
+	dayText.innerHTML = currentDay;
+
 	monthProgressBar.style.width = currentMonthPercentage + '%';
+	monthText.innerHTML = currentMonth + 1;
+
 	decadeProgressBar.style.width = currentDecadePercentage + '%';
+	decadeText.innerHTML = currentYear % 10;
+
 	centuryProgressBar.style.width = currentCenturyPercentage + '%';
+	centuryText.innerHTML = currentYear % 100;
+
 	millenniumProgressBar.style.width = currentMillenniumPercentage + '%';
+	millenniumText.innerHTML = currentYear % 1000;
 }, 1000);
